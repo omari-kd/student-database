ee5330932d0cf18e9864cf32806fbd2772f8d533
Ebenezer Omari <omarieben7@gmail.com>
2025-02-06 19:10:43 +0000

    Save progress: 1049.1

--- insert_data.sh
+++ insert_data.sh
@@ -60,13 +60,13 @@ do
 
     # get major_id
     MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
-    echo $MAJOR_ID
 
     # if not found
     if [[ -z $MAJOR_ID ]]
     then
     # set to null
     MAJOR_ID=null
+     echo $MAJOR_ID
     fi
 
     # insert student
